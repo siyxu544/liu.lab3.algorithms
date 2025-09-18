2f337410cd02d61db0c6fc8f194c14ca2dd1d7fc
ChenzhiNi315 <pinkmannicz@gmail.com>
2025-09-18 18:40:06 +0200

    Update euclidean.R

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,31 +1,42 @@
-#' Find the greatest common divisor of two numbers
+#' Euclidean Algorithm to find Greatest Common Divisor (GCD)
 #'
-#' This function implements the Euclidean algorithm to find the greatest
-#' common divisor (GCD) of two numeric scalars.
-#'
-#' @param a A numeric scalar.
-#' @param b A numeric scalar.
-#' @return The greatest common divisor of a and b.
-#' @references The description and pseudocode for the algorithm can be found
-#'   at \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
+#' @param a A positive integer.
+#' @param b A positive integer.
+#' @return The greatest common divisor of `a` and `b`.
+#' @description This function implements the Euclidean algorithm to find the GCD of two numbers.
+#' @references \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
 #' @export
-#' @examples
-#' euclidean(123612, 13892347912)
-#' euclidean(100, 1000)
-
-euclidean <- function(a, b) {
-
-  # Assert that the arguments are numeric scalars
-  stopifnot(is.numeric(a), length(a) == 1,
-            is.numeric(b), length(b) == 1)
-
-  # Implement the Euclidean algorithm using a while loop
-  while (b != 0) {
-    temp <- b
-    b <- a %% b
-    a <- temp
+euclidean <- function(a, b){
+  # Check if both parameters are numeric scalars
+  if (!is.numeric(a) || length(a) != 1 || !is.numeric(b) || length(b) != 1){
+    stop("Error! Parameters must be numeric scalars")
   }
-
-  # Return the greatest common divisor
+  # Check if both parameters are integers
+  if (a != round(a) || b != round(b)){
+    stop("Error! Parameters must be integers")
+  }
+  # The absolute value of the argument
+  x <- abs(a)
+  y <- abs(b)
+  # The parameters have a situation where zero exists
+  if (a == 0 && b == 0){
+    return(0)
+  }
+  if (a == 0){
+    return(b)
+  }
+  if (b == 0){
+    return(a)
+  }
+  # Euclidean algorithm under normal conditions
+  while (b != 0){
+    rem <- a %% b
+    a <- b
+    b <- rem
+  }
+  # Get the greatest common divisor
   return(a)
 }
+
+euclidean(123612, 13892347912)
+euclidean(100, 1000)
